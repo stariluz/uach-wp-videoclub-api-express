46479d35968e36448361f77b5c713c807911f822
Adrigondo <adrigondo.profesional@gmail.com>
2024-04-12 11:34:41 -0600

    Add: routes

---
+++ routes/waitingLists.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/waitingLists')
+
+router.post('/', controller.create);
+
+router.get('/', controller.list);
+
+router.get('/:id', controller.index);
+
+router.put('/:id', controller.replace);
+
+router.patch('/:id', controller.update);
+
+router.delete('/:id', controller.destroy);
+
+
+module.exports = router;
