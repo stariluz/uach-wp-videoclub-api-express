1dea93355f8036ac5a524f77bfa56750bcddcfdf
Adrigondo <adrigondo.profesional@gmail.com>
2024-04-12 11:24:29 -0600

    Add: Creation of controllers

---
+++ utilities/enums/copyStatus.enum.js
@@ -0,0 +1,5 @@
+const CopyStatusEnum = {
+    'DISPOSABLE': 'DISPOSABLE',
+    'GRATED': 'GRATED',
+    'LOST': 'LOST',
+}
---
+++ utilities/enums/format.enum.js
@@ -0,0 +1,5 @@
+const FormatEnum = {
+    'VHS': 'VHS',
+    'DVD': 'DVD',
+    'BLUE_RAY': 'BLUE_RAY',
+}
